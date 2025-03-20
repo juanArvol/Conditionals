aeff5b37e7ab06a760143e44719310529d40730a
juanArvol <arboledaperdomo@gmail.com>
2025-03-20 17:00:17 -0500

    Conditionals

--- squares/js/squares.js
+++ squares/js/squares.js
@@ -20,28 +20,28 @@ if(squareOne==squareTwo && squareOne==squareThree && squareTwo==squareThree){
 			console.log("All squares are equals");
 }
 else if(squareOne>squareTwo && squareOne>squareThree && squareTwo==squareThree){
-			console.log("Square One is Greatter than square two and three");
+			console.log("Square One is Greatter than square two and three and this are equals");
 }
 else if(squareOne>squareTwo && squareOne==squareThree && squareTwo<squareThree){
-			console.log("Square one and square three are greatter than square two");
+			console.log("Square One and square Three are greatter than square Two and are equals");
 }
 else if(squareOne>squareTwo && squareOne>squareThree && squareTwo>squareThree){
 			console.log("Square One is greatter than Two and Three and Two is greatter than Three but is minor than One");
 }
 else if(squareOne==squareTwo && squareOne>squareThree && squareTwo>squareThree){
-			console.log("Square one and square two are greatter than square three");
+			console.log("Square One and square Two are greatter than square Three and square One and Two are equals");
 }
 else if(squareOne==squareTwo && squareOne<squareThree && squareTwo<squareThree){
-			console.log("Square Three is greatter than One and Two");
+			console.log("Square Three is greatter than One and Two and this are equals");
 }
 else if(squareOne<squareTwo && squareOne<squareThree && squareTwo>squareThree){
-			console.log("Square Two is greatter than square One and square Three");
+			console.log("Square Two is greatter than square One and square Three and square Three is gratter than One but lower than Two");
 }
 else if(squareOne<squareTwo && squareOne>squareThree && squareTwo>squareThree){
-			console.log("Square Two is greatter than Three and One and One is greatter than Three but is minor than Two");
+			console.log("Square Two is greatter than Three and One and One is greatter than Three but is lower than Two");
 }
 else if(squareOne<squareTwo && squareOne<squareThree && squareTwo>squareThree){
-			console.log("Square Two is greatter than Three and One and Three is greatter than One but is minor than Two");
+			console.log("Square Two is greatter than Three and One and Three is greatter than One but is lower than Two");
 }
 else if(squareOne<squareTwo && squareOne<squareThree && squareTwo==squareThree){
 			console.log("Square Two and Three are equals and are greatter than square One");
@@ -49,4 +49,15 @@ else if(squareOne<squareTwo && squareOne<squareThree && squareTwo==squareThree){
 else if(squareOne<squareTwo && squareOne<squareThree && squareTwo>squareThree){
 			console.log("Square Two and Three are equals and are greatter than square One");
 }
-
+else if(squareOne<squareTwo && squareOne<squareThree && squareTwo<squareThree){
+			console.log("Square Three is greatter than One and Two and Two is greatter than One but lower than Three");
+}
+else if(squareOne>squareTwo && squareOne>squareThree && squareTwo<squareThree){
+			console.log("Square One is greatter than Two and Three and Three is greatter than Two but lower than One");
+}
+else if(squareOne>squareTwo && squareOne<squareThree && squareTwo<squareThree){
+			console.log("Square Three is greatter than One and Two and One is greatter than Two but lower than Three");
+}
+else if(squareOne<squareTwo && squareOne==squareThree && squareTwo>squareThree){
+			console.log("Square Two is greatter than One and Three and this are equals");
+}
--- threenumber/js/threenumber.js
+++ threenumber/js/threenumber.js
@@ -5,21 +5,53 @@
 	Date: march 15th 2025
 */
 
-let numberOne=12;
-let numberTwo=15;
-let numberThree=15;
+let numberOne=4;
+let numberTwo=2;
+let numberThree=10;
 
 
-if(numberOne==numberTwo && numberOne==numberThree){
-	console.log("Are equals");
+if(numberOne==numberTwo && numberOne==numberThree && numberTwo==numberThree){
+			console.log("All numbers are equals");
 }
-else if(numberOne>numberTwo){
-	console.log("Number One is greater");
+else if(numberOne>numberTwo && numberOne>numberThree && numberTwo==numberThree){
+			console.log("Number One is Greatter than Number two and three and this are equals");
 }
-else{
-	console.log("Number Two is greater");
+else if(numberOne>numberTwo && numberOne==numberThree && numberTwo<numberThree){
+			console.log("Number One and Number Three are greatter than number Two and are equals");
 }
-
-
-
-
+else if(numberOne>numberTwo && numberOne>numberThree && numberTwo>numberThree){
+			console.log("Number One is greatter than Two and Three and Two is greatter than Three but is minor than One");
+}
+else if(numberOne==numberTwo && numberOne>numberThree && numberTwo>numberThree){
+			console.log("Number One and number Two are greatter than number Three and number One and Two are equals");
+}
+else if(numberOne==numberTwo && numberOne<numberThree && numberTwo<numberThree){
+			console.log("Number Three is greatter than One and Two and this are equals");
+}
+else if(numberOne<numberTwo && numberOne<numberThree && numberTwo>numberThree){
+			console.log("Number Two is greatter than number One and number Three and number Three is gratter than One but lower than Two");
+}
+else if(numberOne<numberTwo && numberOne>numberThree && numberTwo>numberThree){
+			console.log("Number Two is greatter than Three and One and One is greatter than Three but is lower than Two");
+}
+else if(numberOne<numberTwo && numberOne<numberThree && numberTwo>numberThree){
+			console.log("Number Two is greatter than Three and One and Three is greatter than One but is lower than Two");
+}
+else if(numberOne<numberTwo && numberOne<numberThree && numberTwo==numberThree){
+			console.log("Number Two and Three are equals and are greatter than number One");
+}
+else if(numberOne<numberTwo && numberOne<numberThree && numberTwo>numberThree){
+			console.log("Number Two and Three are equals and are greatter than number One");
+}
+else if(numberOne<numberTwo && numberOne<numberThree && numberTwo<numberThree){
+			console.log("Number Three is greatter than One and Two and Two is greatter than One but lower than Three");
+}
+else if(numberOne>numberTwo && numberOne>numberThree && numberTwo<numberThree){
+			console.log("Number One is greatter than Two and Three and Three is greatter than Two but lower than One");
+}
+else if(numberOne>numberTwo && numberOne<numberThree && numberTwo<numberThree){
+			console.log("Number Three is greatter than One and Two and One is greatter than Two but lower than Three");
+}
+else if(numberOne<numberTwo && numberOne==numberThree && numberTwo>numberThree){
+			console.log("Number Two is greatter than One and Three and this are equals");
+}
